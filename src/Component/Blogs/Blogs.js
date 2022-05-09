1ebdaa7c2c975814443afbdc263efbad2655eccd
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-05-09 23:24:59 +0600

    manage item

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>Difference between javascript and nodejs</h1>
-            <p><b>Ans:</b></p>
-            <h1>When should you use nodejs and when should you use mongodb</h1>
+            <h1>Difference between javascript and nodejs.</h1>
+            <p><b>Ans:</b>Javascript is a programming language. And its run into any browser through javascript engine. Its used for client-side activity. Javascript can run in different engine like V8 in google chrome, Spider monkey for firefox etc. On the otherhand Node JS is interpreter for Javascript language which holds huge number of excesses also requires libraries which can easily acceses from javascript.Javascript can run in different engine but Node Js only support V8 engine so it mainly used in chrome browser and it is written in C++. Thats why it easily run. </p>
+            <br />
+            <h1>When should you use nodejs and when should you use mongodb?</h1>
             <p><b>Ans:</b></p>
             <h1>Differences between sql and nosql databases.</h1>
-            <p><b>Ans:</b></p>
+            <p><b>Ans:</b>SQL is relational database and NoSQL stands for non relational database. Vertically scalable file used in SQL. Also the databases are table based. On the other hand NonSQL use horizontally scalable file and here the database are document type, graph, wide column. Sql use predefined schema and structured query language so that its better for multi row transictions, </p>
             <h1>What is the purpose of jwt and how does it work</h1>
             <p><b>Ans:</b></p>
         </div>
--- src/Component/ManageItem/ManageItem.js
+++ src/Component/ManageItem/ManageItem.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import { TrashIcon } from "@heroicons/react/outline";
+import React from "react";
+import useItems from "../Hooks/useItems";
 
 const ManageItem = () => {
-    return (
-        <div>
-            
+  const [items, setItems] = useItems();
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure?");
+    if (proceed) {
+      const url = `http://localhost:5000/item/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const remaining = items.filter((item) => item._id !== id);
+          setItems(remaining);
+        });
+    }
+  };
+  return (
+    <div>
+      {items.map((item) => (
+        <div className="grid justify-items-center border" key={item._id}>
+          
+          <img src={item.image} alt="" />
+          <h2 className="mb-2"><span className="font-bold">Name:</span> {item.productName}</h2>
+          <p className="mb-2"><span className="font-bold">Price:</span> {item.price}</p>
+          <p className="mb-2"><span className="font-bold">Quantity:</span> {item.quantity}</p>
+          <p className="mb-2"><span className="font-bold">Supplier Name:</span> {item.supplierName}</p>
+          <button className="flex items-center mb-5 bg-blue-500 text-white px-5 py-2" onClick={() => handleDelete(item._id)}>
+          <TrashIcon className="h-5 w-5"></TrashIcon>
+          <p>Delete</p>
+          </button>
+          
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ManageItem;
